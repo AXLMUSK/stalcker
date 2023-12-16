f4bf968f841b917e5b57160f61c526c4ebd3977d
dthtec-axl <90142286+AXLMUSK@users.noreply.github.com>
2023-12-16 12:07:19 -0300

    Delete stalcker directory

--- stalcker/redes_sociais/salvar_resultados.py
+++
@@ -1,5 +0,0 @@
-# redes_sociais/salvar_resultados.py
-def salvar_resultados(resultados):
-    with open('resultados_redes_sociais.txt', 'w') as arquivo:
-        for resultado in resultados:
-            arquivo.write(resultado + '\n')
